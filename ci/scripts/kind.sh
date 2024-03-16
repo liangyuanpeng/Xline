8604b929d3a080b85ae9ba5b856966bb5d06f76d
Lan Liang <gcslyp@gmail.com>
2024-03-16 16:51:36 +0800

    u

--- ci/scripts/kind.sh
+++ ci/scripts/kind.sh
@@ -4,14 +4,16 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-
 wget -q https://github.com/kubernetes-sigs/kind/releases/download/v0.22.0/kind-linux-amd64
 chmod +x kind-linux-amd64 && mv kind-linux-amd64 /usr/local/bin/kind
 
 K8SVERSION=${K8SVERSION:-"v1.25.3"}
 WORKSPACE=$PWD
 
-sed -i  's/K8SVERSION/'$K8SVERSION'/g' $WORKSPACE/ci/artifact/kind.yaml
-sed -i  's#WORKSPACE#'$WORKSPACE'#g' $WORKSPACE/ci/artifact/kind.yaml
-kind create cluster --retain --config $WORKSPACE/ci/artifact/kind.yaml
+envsubst < $WORKSPACE/ci/artifact/kind.yaml > kind create cluster -v7 --retain --wait 1m --config -
+
+# sed -i  's/K8SVERSION/'$K8SVERSION'/g' $WORKSPACE/ci/artifact/kind.yaml
+# sed -i  's#WORKSPACE#'$WORKSPACE'#g' $WORKSPACE/ci/artifact/kind.yaml
+#kind create cluster --retain --config $WORKSPACE/ci/artifact/kind.yaml
+
 kubectl wait node --all --for condition=ready
