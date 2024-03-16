974572d2ce24aa6e770570abbfe90e2f00d0875e
Lan Liang <gcslyp@gmail.com>
2024-03-16 17:23:02 +0800

    u

--- ci/scripts/kind.sh
+++ ci/scripts/kind.sh
@@ -8,14 +8,10 @@ wget -q https://github.com/kubernetes-sigs/kind/releases/download/v0.22.0/kind-l
 chmod +x kind-linux-amd64 && mv kind-linux-amd64 /usr/local/bin/kind
 
 K8SVERSION=${K8SVERSION:-"v1.27.3"}
-WORKSPACE=$PWD
-
-echo $WORKSPACE
+WORKSPACE=$PWD envsubst < ci/artifact/kind.yaml
 echo "================================"
 
-envsubst < ci/artifact/kind.yaml
-
-envsubst < ci/artifact/kind.yaml | kind create cluster -v7 --retain --wait 4m --config -
+WORKSPACE=$PWD envsubst < ci/artifact/kind.yaml | kind create cluster -v7 --retain --wait 4m --config -
 
 # sed -i  's/K8SVERSION/'$K8SVERSION'/g' $WORKSPACE/ci/artifact/kind.yaml
 # sed -i  's#WORKSPACE#'$WORKSPACE'#g' $WORKSPACE/ci/artifact/kind.yaml
