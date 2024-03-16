16a5fd27e2387ce8480719c0ce568207c688a8f5
Lan Liang <gcslyp@gmail.com>
2024-03-16 15:29:00 +0800

    u

--- ci/scripts/kind.sh
+++ ci/scripts/kind.sh
@@ -11,7 +11,7 @@ chmod +x kind-linux-amd64 && mv kind-linux-amd64 /usr/local/bin/kind
 K8SVERSION=${K8SVERSION:-"v1.25.3"}
 WORKSPACE=$PWD
 
-sed -i  's/K8SVERSION/'$K8SVERSION'/g' $WORKSPACE/.github/scripts/kind.yaml
-sed -i  's#WORKSPACE#'$WORKSPACE'#g' $WORKSPACE/.github/scripts/kind.yaml
-kind create cluster --config $WORKSPACE/.github/scripts/kind.yaml
+sed -i  's/K8SVERSION/'$K8SVERSION'/g' $WORKSPACE/ci/artifact/kind.yaml
+sed -i  's#WORKSPACE#'$WORKSPACE'#g' $WORKSPACE/ci/artifact/kind.yaml
+kind create cluster --config $WORKSPACE/ci/artifact/kind.yaml
 kubectl wait node --all --for condition=ready
